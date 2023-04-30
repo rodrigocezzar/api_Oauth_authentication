fd46d6c3c8478de44afca1ac28557746b675416f
Rodrigo de Abreu Cezar <rodrigodeabreucezar@MacBook-Air-de-Rodrigo.local>
2023-04-30 19:27:42 -0300

    implementation of the article controller, serializer and its validation tests

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,6 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :slug, presence: true, uniqueness: true
+
+  scope :recent, -> { order(created_at: :desc) }
 end
